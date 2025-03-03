1175a083d40d8d924d54fb0fdc5d3663cea9d725
Sherif Mesbah <sherifmesbah4@gmail.com>
2025-03-03 17:44:47 +0200

    Setup backend environment

---
+++ src/Smart Label.Infrastructure/Persistence/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Smart_Label.Infrastructure.Persistence.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+
+    }
+}
